b91255a7f90b65eb6932a87555dd6f5b67d54427
CuongCoder <nguyenquoccuong07122004@gmail.com>
2025-03-11 23:06:02 +0700

    demo new update

--- app/component/social/social.jsx
+++ app/component/social/social.jsx
@@ -1,37 +1,6 @@
-import { useState } from "react"
-
-import CreateMember from "./createMember"
-import TableFilter from "./tableFilter"
-import Navbar from "../home/navbar";
-
-import Router from "@/app/router/router";
 
 export default function Social() {
-    const { navigateToHome } = Router();
-
-    const [filter, setFilter] = useState(false);
-    const [filterValue, setFilterValue] = useState({
-        name: [],
-        code: [],
-    });
-
     return (
-        <main id="main">
-            <div id="header">
-                <Navbar handleReturn={navigateToHome} />
-            </div>
-            <CreateMember
-                sizeDevice={size}
-                onFilter={filter}
-                setFilter={() => setFilter(!filter)}
-                filterValue={filterValue}
-            />
-            <TableFilter
-                sizeDevice={size}
-                onFilter={filter}
-                setFilter={() => setFilter(false)}
-                handleFilter={(value) => setFilterValue({ name: value.name, code: value.code })}
-            />
-        </main>
+        <div>Social</div>
     )
 }
