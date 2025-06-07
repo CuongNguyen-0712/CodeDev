4faff0ba2afef172df6ae0db73ef8b2453a5c624
CuongCoder <nguyenquoccuong07122004@gmail.com>
2025-06-07 11:43:42 +0700

    fix missing suspense to deploy

--- app/home/project/page.jsx
+++ app/home/project/page.jsx
@@ -1,9 +1,14 @@
 'use client'
 import Project from "@/app/component/project/project";
+import { Suspense } from "react";
+
+import { LoadingContent } from "@/app/component/ui/loading";
 
 export default function Page() {
     return (
-        <Project />
+        <Suspense fallback={<LoadingContent />}>
+            <Project />
+        </Suspense>
     )
 }
 
