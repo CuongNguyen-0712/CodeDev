7012bd4ff9189368d69c82675dbd02765bb36ab2
CuongCoder <nguyenquoccuong07122004@gmail.com>
2026-07-03 09:25:38 +0700

    fix: deploy prerender

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,11 +1,17 @@
+import { Suspense } from "react";
+
+import { LoadingRedirect } from "@/app/component/ui/loading";
+
 import HomeLayout from "../layout/homeLayout"
 
 import Blog from "../component/blog/blog"
 
 export default function Page() {
     return (
-        <HomeLayout>
-            <Blog />
-        </HomeLayout>
+        <Suspense fallback={<LoadingRedirect />}>
+            <HomeLayout>
+                <Blog />
+            </HomeLayout>
+        </Suspense>
     );
 }
